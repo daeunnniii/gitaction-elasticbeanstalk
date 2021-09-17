40fbb6f4ccef33637af410871bc2c7d05e6fa443
daeunnniii <storyda1@naver.com>
2021-09-18 01:39:46 +0900

    GitHub Action을 이용한 배포

---
+++ application.py
@@ -0,0 +1,33 @@
+import boto3
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+# cors
+cors = CORS(application, resources={r"/*": {"origins": "*"}})
+
+@application.route('/')
+def main():
+    return "Elasticbeanstalk 연동하기"
+
+@application.route('/fileupload', methods=['POST'])
+def file_upload():
+    file = request.files['file']
+    s3 = boto3.client('s3',
+                      aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+                      aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+                      )
+    s3.put_object(
+        ACL="public-read",
+        Bucket=os.environ["BUCKET_NAME"],
+        Body=file,
+        Key=file.filename,
+        ContentType=file.content_type
+    )
+
+    return jsonify({'result': 'success'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
